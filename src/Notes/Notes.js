1957a32c11bc327d87df8dc68ee86aeb87955e0b
Jaclyn <jaclyndshahan@gmail.com>
2019-07-23 19:48:43 -0500

    making sticky notes

--- src/Notes/Notes.js
+++ src/Notes/Notes.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Icon } from 'antd';
+import './Notes.css';
 
 class Notes extends Component {
     constructor() {
@@ -11,6 +13,11 @@ class Notes extends Component {
     render() {
         return(
             <div>
+                <form className="noteForm">
+                    <Icon className="addIcon" type="plus"/>
+                    <h1>Things To Do</h1>
+                    <p>Make Notes in here so I know what I am doing.</p>
+                </form>
 
             </div>
         )
