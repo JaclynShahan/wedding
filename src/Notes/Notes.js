56335f72b8e8dc42a338400ef1aca3da8b482d26
Jaclyn <jaclyndshahan@gmail.com>
2019-07-26 23:40:39 -0500

    working front end note

--- src/Notes/Notes.js
+++ src/Notes/Notes.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react';
 import { Icon } from 'antd';
 import './Notes.css';
+import Axios from 'axios';
 
 class Notes extends Component {
     constructor() {
         super() 
         this.state = {
-
+            note: "",
+            notesArr: []
         }
     }
 
+    componentDidMount = () => {
+        Axios.get(`/api/getNote`).then(resp => {
+          console.log(resp) 
+          this.setState({notesArr: resp.data}) 
+        })
+    }
+
+    pendingNote = e => {
+        e.preventDefault()
+        Axios.post(`/api/postNote`, {
+           note: this.state.note 
+        }).then(resp => {
+           this.onClear()
+           console.log(resp)
+           this.setState({notesArr: resp.data}) 
+        })
+    }
+
+    onClear = () => {
+        this.setState({
+            note: ''
+        })
+    }
+
+    updateNote = (note) => {
+        this.setState({note})
+    }
     render() {
         return(
             <div>
-                <form className="noteForm">
-                    <Icon className="addIcon" type="plus"/>
-                    <h1>Things To Do</h1>
-                    <p>Make Notes in here so I know what I am doing.</p>
-                </form>
+                <form>
+           <input 
+                value={this.state.pendingNote}
+                type="text"
+                placeholder="What I Need To Do..."
+                />
+             <button type="submit"><Icon className="addIcon" type="plus"/></button>
+              </form>
+                <div className="noteForm">
+                {this.state.pendingNote}
+                </div>
 
             </div>
         )
